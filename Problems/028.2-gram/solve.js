cbc13e291b7cba3170560ebe33ee02cab51ad04d
Jihoon Chae <hoonn94@naver.com>
2022-05-30 23:36:43 +0900

    Feat : 026~029번 문제 추가

--- Problems/028.2-gram/solve.js
+++ Problems/028.2-gram/solve.js
@@ -5,19 +5,19 @@
 // 예를 들어 'Javascript'를 2-gram으로 반복해 본다면 다음과 같은 결과가 나옵니다.
 
 
-**입력**
-Javascript
+// **입력**
+// Javascript
 
-**출력**
-J a
-a v
-v a
-a s
-s c
-c r
-r i
-i p
-p t
+// **출력**
+// J a
+// a v
+// v a
+// a s
+// s c
+// c r
+// r i
+// i p
+// p t
 
 
 // 입력으로 문자열이 주어지면 **2-gram**으로 출력하는 프로그램을 작성해 주세요.
