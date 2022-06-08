041a22cc131f5d00213d0bf757aae34490619330
subincdev <subinc.dev@gmail.com>
2022-06-08 23:11:45 +0900

    Docs : 잘못된 파일 경로 수정
    11,12,13번 문제를 Problems js파일에서 풀어서 해당 부분을 삭제하였습니다

--- Problems/011.for/solve.js
+++ Problems/011.for/solve.js
@@ -9,9 +9,3 @@ let s = 0;
 
 console.log(s);
 */
-
-let s = 0;
-for (let i = 1; i <= 100; i++) {
-  s += i;
-}
-console.log(s);
--- Problems/012.class/solve.js
+++ Problems/012.class/solve.js
@@ -15,18 +15,3 @@ x.attack();
 545 210 10
 파이어볼
 */
-
-class Wizard {
-  constructor(health, mana, armor) {
-    this.health = health;
-    this.mana = mana;
-    this.armor = armor;
-  }
-  attack() {
-    console.log("파이어볼");
-  }
-}
-
-const x = new Wizard(545, 210, 10);
-console.log(x.health, x.mana, x.armor);
-x.attack();
