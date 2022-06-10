aec5c367099fdda62fbc87efc19c699c86b32983
yeeed711 <rladpwl910@naver.com>
2022-06-10 09:57:44 +0900

    Solve: 9번 문제 해결

---
+++ Challenge/YejiKim/009.concat/solve.js
@@ -0,0 +1,22 @@
+var year = "2019";
+var month = "04";
+var day = "26";
+var hour = "11";
+var minute = "34";
+var second = "27";
+
+var result = year.concat(
+  "/",
+  month,
+  "/",
+  day,
+  " ",
+  hour,
+  ":",
+  minute,
+  ":",
+  second
+);
+console.log(result);
+
+// '2019/04/26 11:34:27'
