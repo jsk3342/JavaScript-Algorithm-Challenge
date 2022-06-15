74ba4b57eed0936707373fc8d474f2d93d605088
subincdev <subinc.dev@gmail.com>
2022-06-16 06:56:17 +0900

    Solve: 19번 문제 풀이

---
+++ Challenge/SubinChoi/019.square/solve.js
@@ -0,0 +1,8 @@
+// 문제19 : 제곱을 구하자
+
+// 공백으로 구분하여 두 숫자 a와 b가 주어지면, a의 b승을 구하는 프로그램을 작성하세요.
+
+const [num1, num2] = prompt("두 숫자를 입력해주세요")
+  .split(" ")
+  .map((ele) => parseInt(ele));
+console.log(num1 ** num2);
