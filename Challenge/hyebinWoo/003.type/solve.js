a9f30c6735bcf4cb58b01d2c1f6836b9d450ddd7
Hyebin-Woo <dev.hyebin@gmail.com>
2022-06-18 11:51:21 +0900

    Solve:문제4~9번해결

--- Challenge/hyebinWoo/003.type/solve.js
+++ Challenge/hyebinWoo/003.type/solve.js
@@ -2,10 +2,14 @@
 
 // 다음 출력 값으로 올바른 것은?
 
-var arr = [100, 200, 300];
-console.log(typeof(arr));
+let arr = [100, 200, 300];
+console.log(typeof arr);
 
 // 1)  undefined
 // 2)  string
 // 3)  number
 // 4)  object
+
+// 4
+// 자바스크립트에서 배열은 object
+// undefined, string, number 는 모두 기본 자료형(primitive type)이다.
