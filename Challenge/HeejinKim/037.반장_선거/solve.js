195dfc95328dc30b6277065667b98239e3bfab4f
히징 <khj16355@naver.com>
2022-06-19 20:21:53 +0900

    Solve : 037 문제 해결

---
+++ Challenge/HeejinKim/037.반장_선거/solve.js
@@ -0,0 +1,28 @@
+/*
+# 문제37 : 반장 선거
+
+새 학기를 맞아 호준이네 반은 반장 선거를 하기로 했습니다.  그런데 표를 하나씩 개표하는 과정이 너무 번거롭게 느껴진 당신은 **학생들이 뽑은 후보들을 입력받으면 뽑힌 학생의 이름과 받은 표 수를 출력하는 프로그램**을 작성하기로 하였습니다.
+
+```jsx
+**입력**
+원범 원범 혜원 혜원 혜원 혜원 유진 유진
+
+**출력**
+혜원(이)가 총 4표로 반장이 되었습니다.
+```
+*/
+
+let people = prompt("후보를 입력해주세요").split(" ");
+let result = {};
+let winner = "";
+
+for (let i = 0; i < people.length; i++) {
+  let val = people[i];
+  result[val] = result[val] === undefined ? 1 : (result[val] = result[val] + 1);
+}
+
+winner = Object.keys(result).reduce(function (a, b) {
+  return result[a] > result[b] ? a : b;
+});
+
+console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다`);
