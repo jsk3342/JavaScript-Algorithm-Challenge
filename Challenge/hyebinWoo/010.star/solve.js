868b1eb71e291ac8c9235c6bb4eeaff10bb82610
Hyebin-Woo <dev.hyebin@gmail.com>
2022-06-21 23:57:20 +0900

    Solve:문제11~12번해결

--- Challenge/hyebinWoo/010.star/solve.js
+++ Challenge/hyebinWoo/010.star/solve.js
@@ -19,40 +19,17 @@
 const num = prompt("숫자를 입력하세요.");
 let star = "";
 
-// for (let i = 1; i <= num; i++) {
-//   for (let j = 1; j <= num - i; j++) {
-//     star += " ";
-//   }
-//   for (let z = 1; z <= 2 * i - 1; z++) {
-//     star += "*";
-//   }
-//   star += "\n";
-// }
 for (let i = 1; i <= num; i++) {
-  console.log(" ".repeat(num - i) + "*".repeat(2 * i - 1));
+  for (let j = 1; j <= num - i; j++) {
+    star += " ";
+  }
+  for (let z = 1; z <= 2 * i - 1; z++) {
+    star += "*";
+  }
+  star += "\n";
 }
-// console.log(star);
-
-// function starTree1(num) {
-//   let star = "";
-
-//   for (let i = 1; i <= num; i++) {
-//     for (let j = 1; j <= num - i; j++) {
-//       star += " ";
-//     }
-//     for (let z = 1; z <= 2 * i - 1; z++) {
-//       star += "*";
-//     }
-//     star += "\n";
-//   }
-//   console.log(star);
+// for (let i = 1; i <= num; i++) {
+//   console.log(" ".repeat(num - i) + "*".repeat(2 * i - 1));
 // }
-// starTree1(5);
+console.log(star);
 
-// function starTree2(num) {
-//   // repeat() 메서드는 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열을 반환합니다.
-//   for (let i = 1; i <= num; i++) {
-//     console.log(" ".repeat(num - i) + "*".repeat(2 * i - 1));
-//   }
-// }
-// starTree2(5);
