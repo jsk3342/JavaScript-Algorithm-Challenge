5952440224c3308d58b5a7bb3541a933bea0784c
Lim Heelae <ehhwll@hanmail.net>
2022-08-13 02:06:58 +0900

    Solve: 052, 053

---
+++ Challenge/HeelaeLim/052.QuickSort/solve.js
@@ -0,0 +1,53 @@
+/*
+# 문제52 : quick sort
+
+다음 빈 칸을 채워 퀵 정렬을 완성해주세요.
+
+```js
+function quickSort(arr){
+  if (arr.length <= 1){
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i=1; i<arr.length; i++){
+    if(빈칸을 채워주세요){
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return 빈칸을 채워주세요
+}
+
+const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+
+console.log(quickSort(array));
+```
+*/
+
+function quickSort(arr){
+  if (arr.length <= 1){
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i=1; i<arr.length; i++){
+    if(arr[i] < pivot){
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+}
+
+const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+
+console.log(quickSort(array));
